d689fd4c6daa9d4ad06eacdd5408e43b91db8952
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-02-11 13:49:13 -0500

    Create Shelter class.

--- src/com/company/Shelter.java
+++ src/com/company/Shelter.java
@@ -1,4 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Shelter {
+
+    private ArrayList<Animal> animals;
+
+    public Shelter(ArrayList<Animal> animals) {
+        this.animals = new ArrayList<Animal>();
+    }
+
+    public void addAnAnimal(Animal animal){
+        animals.add(animal);
+        System.out.println("You've added a(n) to the shelter:  " + animal.getName());
+        System.out.println(animals.size());
+    }
+
+    public void removeAnimal(String idNumber){
+        for (int i = 0; i < animals.size(); i++){
+            if (idNumber.equalsIgnoreCase(animals.get(i).getIdNumber())){
+                System.out.println("You just removed an animal from the shelter.  The information is:  ");
+                Animal removedAnimal = animals.remove(i);
+                removedAnimal.showAllAnimals();
+                // TODO Add showAllAnimals
+            } else {
+                System.out.println("Sorry.  Couldn't find that animal in the shelter.");
+            }
+        }
+    }
+
 }
